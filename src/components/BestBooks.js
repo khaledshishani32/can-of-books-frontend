2941c21a10595b46508a7130ec7e20392ebda8ea
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-21 21:32:32 +0300

    lab12

---
+++ src/components/BestBooks.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { withAuth0 } from "@auth0/auth0-react";
+import Book from "./Book";
+export class BestBooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: this.props.auth0.user.name,
+      userEmail: this.props.auth0.user.email,
+      userPicture: this.props.auth0.user.picture,
+      serverUrl: process.env.REACT_APP_SERVER_URL,
+      booksData: [],
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(`http://localhost:8080/books?email=${this.state.userEmail}`)
+      .then((response) => {
+        this.setState({
+            booksData: response.data[0].books,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+      
+       {
+           this.state.booksData.length>0 && 
+         
+           this.state.booksData.map((obj , index)=><Book key={this.state.booksData[index].name} booksData={this.state.booksData[index]}/>)
+        }
+      </div>
+    );
+  }
+}
+
+export default withAuth0(BestBooks);
